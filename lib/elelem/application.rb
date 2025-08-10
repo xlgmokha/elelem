0880f854c22acd0985bc3be559df84a3744f7eab
mo khan <mo@mokhan.ca>
2025-08-10 00:54:01 -0600

    style: format the options

--- lib/elelem/application.rb
+++ lib/elelem/application.rb
@@ -3,14 +3,30 @@
 module Elelem
   class Application < Thor
     desc "chat", "Start the REPL"
-    method_option :help, aliases: "-h", type: :boolean, desc: "Display usage information"
-    method_option :host, aliases: "--host", type: :string, desc: "Ollama host",
-                         default: ENV.fetch("OLLAMA_HOST", "localhost:11434")
-    method_option :model, aliases: "--model", type: :string, desc: "Ollama model",
-                          default: ENV.fetch("OLLAMA_MODEL", "gpt-oss")
-    method_option :token, aliases: "--token", type: :string, desc: "Ollama token",
-                          default: ENV.fetch("OLLAMA_API_KEY", nil)
-    method_option :debug, aliases: "--debug", type: :boolean, desc: "Debug mode", default: false
+    method_option :help,
+      aliases: "-h",
+      type: :boolean,
+      desc: "Display usage information"
+    method_option :host,
+      aliases: "--host",
+      type: :string,
+      desc: "Ollama host",
+      default: ENV.fetch("OLLAMA_HOST", "localhost:11434")
+    method_option :model,
+      aliases: "--model",
+      type: :string,
+      desc: "Ollama model",
+      default: ENV.fetch("OLLAMA_MODEL", "gpt-oss")
+    method_option :token,
+      aliases: "--token",
+      type: :string,
+      desc: "Ollama token",
+      default: ENV.fetch("OLLAMA_API_KEY", nil)
+    method_option :debug,
+      aliases: "--debug",
+      type: :boolean,
+      desc: "Debug mode",
+      default: false
     def chat(*)
       if options[:help]
         invoke :help, ["chat"]
