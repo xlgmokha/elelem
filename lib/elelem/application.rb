7fc59356dfec373a606f9565f68a52c4ba6fbc79
mo khan <mo@mokhan.ca>
2025-11-14 11:20:28 -0700

    feat: add files subcommand to produce Claude XML

--- lib/elelem/application.rb
+++ lib/elelem/application.rb
@@ -13,7 +13,6 @@ module Elelem
                   type: :string,
                   desc: "Ollama model",
                   default: ENV.fetch("OLLAMA_MODEL", "gpt-oss")
-
     def chat(*)
       client = Net::Llm::Ollama.new(
         host: options[:host],
@@ -22,6 +21,20 @@ module Elelem
       say "Agent (#{options[:model]})", :green
       agent = Agent.new(client, Toolbox.new)
       agent.repl
+    end
+
+    desc "files", "Generate CXML of the files"
+    def files
+      puts '<documents>'
+      $stdin.read.split("\n").map(&:strip).reject(&:empty?).each_with_index do |file, i|
+        next unless File.file?(file)
+
+        puts "  <document index=\"#{i + 1}\">"
+        puts "    <source><![CDATA[#{file}]]></source>"
+        puts "    <document_content><![CDATA[#{File.read(file)}]]></document_content>"
+        puts "  </document>"
+      end
+      puts '</documents>'
     end
 
     desc "version", "The version of this CLI"
