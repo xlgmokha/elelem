f47b5b129424f260e208c19ba5f134628b632210
mo khan <mo@mokhan.ca>
2026-01-21 19:18:17 -0700

    fix: print tool header before verification command

--- lib/elelem/plugins/verify.rb
+++ lib/elelem/plugins/verify.rb
@@ -34,7 +34,7 @@ module Elelem
     ) do |a|
       path = a["path"]
       Verifiers.for(path).inject({verified: []}) do |memo, cmd|
-        $stdout.puts "\n  -> verify: #{cmd}"
+        $stdout.puts toolbox.header("execute", { "command" => cmd })
         v = toolbox.run("execute", { "command" => cmd })
         return v.merge(path: path, command: cmd) if v[:exit_status] != 0
 
